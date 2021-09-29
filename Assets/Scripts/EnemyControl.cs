38fc9ca75d94798ea4b95a3738896836c89b5161
bilgecakar <bilgecakar123@outlook.com>
2021-09-29 17:32:51 +0300

    Enemy düştükten sonra yok olaması eklenildi.

--- Assets/Scripts/EnemyControl.cs
+++ Assets/Scripts/EnemyControl.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class EnemyControl : MonoBehaviour
 {
+    [SerializeField] float rotatiom = 50f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,6 @@ public class EnemyControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(0, 0, Time.deltaTime * rotatiom);
     }
 }
