e77c457d619d2c4757114c9ef12e776f38a4c146
bilgecakar <bilgecakar123@outlook.com>
2021-09-30 21:16:31 +0300

    Red enemy eklendi

--- Assets/Scripts/RedEnemyControl.cs
+++ Assets/Scripts/RedEnemyControl.cs
@@ -2,18 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyControl : MonoBehaviour
+public class RedEnemyControl : MonoBehaviour
 {
     [SerializeField] float rotation = 50f;
-    [SerializeField] GameObject enemy;
+    [SerializeField] GameObject redEnemy;
     [SerializeField] float repeatTime = 10f;
     GameObject player;
-        // Start is called before the first frame update
+    // Start is called before the first frame update
     void Start()
     {
-        
+
         InvokeRepeating("onSpawn", 1f, repeatTime);
-  
+
 
 
     }
@@ -21,7 +21,7 @@ public class EnemyControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
+
         transform.Rotate(0, 0, Time.deltaTime * rotation);
 
 
@@ -31,7 +31,7 @@ public class EnemyControl : MonoBehaviour
     {
         player = GameObject.FindWithTag("Player");
         var position = new Vector2(Random.Range(-4.0f, 4.0f), Random.Range(12.0f, 13.0f));
-        Instantiate(enemy, position, Quaternion.identity);
+        Instantiate(redEnemy, position, Quaternion.identity);
         transform.Translate(-player.transform.position.x * Time.deltaTime, -player.transform.position.y * Time.deltaTime, 0);
 
     }
