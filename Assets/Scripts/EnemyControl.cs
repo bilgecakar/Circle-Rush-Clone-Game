a0a899d7848cf552771423c7ba282e77f4dd6d12
bilgecakar <bilgecakar123@outlook.com>
2021-09-30 14:19:46 +0300

    Arka plan müziği eklenildi ve enemy random spawn oluşturuldu.

--- Assets/Scripts/EnemyControl.cs
+++ Assets/Scripts/EnemyControl.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 
 public class EnemyControl : MonoBehaviour
 {
-    [SerializeField] float rotatiom = 50f;
+    [SerializeField] float rotation = 50f;
+    [SerializeField] GameObject enemy;
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("onSpawn", 3f, 3f);
+      
     }
 
     // Update is called once per frame
     void Update()
+    {     
+        transform.Rotate(0, 0, Time.deltaTime * rotation);
+        
+    }
+
+    void onSpawn()
     {
-        transform.Rotate(0, 0, Time.deltaTime * rotatiom);
+        var position = new Vector3(Random.Range(-8.0f, 8.0f), Random.Range(9.0f, 15.0f), 0);
+        Instantiate(enemy, position, Quaternion.identity);
     }
 }
